e495d5d96cbaed7b97b3c2a37f65b938e4ad0e57
cbbrainerd <cbbrainerd@ucdavis.edu>
2015-02-07 20:30:45 -0800

    Done???

--- Directory.py
+++ Directory.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import operator
 from optparse import OptionParser
 import os
 
@@ -12,28 +13,32 @@ def human(siz):
         siz = siz/1024
         j += 1
         if (j > 9):
-            return("This file is too big to exist.")
+            return("This file is way too big to actually exist.")
     return(str(siz/10)+"."+str(siz%10)+markers[j])    
 
 def manual():
     #Manual
     print("Lists the contents of a selected directory. Note that the types only function properly on a Linux machine.")
     print("Available options:")
-    print("-d[directory]: required. Both absolute paths and paths relative to the current working directory are acceptable. However, using bash shortcuts such as ~user does not function correctly.")
+    print("-d[directory]: required. Both absolute paths and paths relative to the current working directory are acceptable.")
     print("-a: Sorts by size in ascending order. By default, this program sorts by size in descending order.")
-    print("-t: Sorts by type. If both this and -a are selected, -a is ignored")
+    print("-t: Sorts by type. If both this and -a are selected, -a is ignored.")
+    print("-r: Run as root. Requires root priveleges, of course.")
     quit()
 
 def whatTypeAmI(path):
     if (os.path.isdir(path)):
         return("Directory")
     elif (os.access(path,os.X_OK)):
-        return("Executable")
+        return("Executable") #This really returns whether execution is permitted for the current user, but that's what matters anyway, right?
     else:
         return("File")
 
 
 def printDir(direct):
+    if(not (os.path.isdir(direct))):
+        print "Requested directory \""+direct+"\" is not a valid directory. If you need help, run this command with no flags to see the help file."
+        quit()
     #Print directory
     a= os.listdir(direct)
     if (options.typee):
@@ -57,13 +62,25 @@ p.add_option("-t", action="store_true", dest="typee", default=False)
 p.add_option("-r", action="store_false", dest="notroot", default=True)
 (options, args) = p.parse_args()
 
-if ((os.geteuid() == 0) and notroot):
-    print("Why are you running this as root?")
-    print("If you meant to do this as root, please run with the flag -r")
+if ((os.geteuid() == 0) and options.notroot):
+    print "Why are you running this as root?"
+    print "If you meant to do this as root, please run with the flag -r."
     quit()
-if (options.filename == "/dev/null"):
+if ((os.geteuid() != 0) and not options.notroot):
+    print "This requires root priveleges."
+    print "Please run as root or run without the -r flag."
+    quit()
+if ((options.filename == "/dev/null") or (len(options.filename) == 0)):
     manual()
-if (os.path.isdir(options.filename)):
-    printDir(options.filename)
-else:
-    print("Directory does not exist. For help, enter no flags at the command line.")
+if (options.filename[0] == "~"):
+    if (len(options.filename) == 1):
+        printDir(os.getenv("HOME"))
+    elif (options.filename[1] == "/"):
+        printDir(os.getenv("HOME"))
+    else:
+        userhome = (options.filename.split("~",1)[1].split("/",1))
+        if ((len(userhome) == 2) and (len(userhome[1]) != 0)):
+            printDir("/home/"+userhome[0]+"/"+options.filename.split("/",1)[1])
+        else:
+            printDir("/home/"+userhome[0])
+printDir(options.filename)
